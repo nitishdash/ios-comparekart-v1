7d13b99d73b54dc4fe4c6cf08d8191dea8a7a61f
Nitish Dash <nitishdash@Nitishs-Mac.local>
2017-04-16 05:47:54 -0700

    Initial Commit

---
+++ CompareKart-v1Tests/CompareKart_v1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CompareKart_v1Tests.swift
+//  CompareKart-v1Tests
+//
+//  Created by Nitish Dash on 16/04/17.
+//  Copyright © 2017 Nitish Dash. All rights reserved.
+//
+
+import XCTest
+@testable import CompareKart_v1
+
+class CompareKart_v1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
