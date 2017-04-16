ac2dfecb6bc11ab1d95278fc09f0b3c9f0df54a8
Nitish Dash <nitishdash@Nitishs-Mac.local>
2017-04-16 07:27:20 -0700

    Original Commit

--- CompareKart-v1/ViewController.swift
+++ CompareKart-v1/ViewController.swift
@@ -7,19 +7,151 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
+import Charts
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController , ChartViewDelegate {
+
+    @IBOutlet weak var lineChartView: LineChartView!
+    
+    let months = ["Jan" , "Feb", "Mar", "Apr", "May", "June", "July", "August", "Sept", "Oct", "Nov", "Dec"]
+    
+    let dollars1 = [1453.0,2352,5431,1442,5451,6486,1173,5678,9234,1345,9411,2212]
+    
+    var priceData = [0.00]
+    
+    var datesData = [""]
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+//        http://localhost/laravel/blog/public/api/product?pid=MOBEQHMGED7F9CZ2        
+        
+        self.priceData.removeAll()
+        
+        self.datesData.removeAll()
+        
+        
+        Alamofire.request("http://192.168.83.1/laravel/blog/public/api/product?pid=MOBEQHMGED7F9CZ2").responseJSON { response in
+            
+            if let value = response.result.value {
+                let json = JSON(value)
+                
+                print(json)
+                
+                 let count: Int? = json.array?.count
+                
+                for index in 0...count!-1 {
+                    
+                    self.priceData.append(json[index]["price"].doubleValue)
+                    
+                    self.datesData.append(json[index]["Name"].stringValue)
+                }
+                
+                
+                // 1
+                self.lineChartView.delegate = self
+                // 2
+                self.lineChartView.chartDescription?.text = "Tap node for details"
+                // 3
+                self.lineChartView.chartDescription?.textColor = UIColor.black
+                self.lineChartView.gridBackgroundColor = UIColor.darkGray
+                // 4
+                self.lineChartView.noDataText = "No data provided"
+                // 5
+                self.setChartData(months: self.datesData)
+                
+                
+
+            }
+        }
+        
+        
+        
+        
+
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    func setChartData(months : [String]) {
+        
+        // 1 - creating an array of data entries
+        var yVals1 : [ChartDataEntry] = [ChartDataEntry]()
+        
+        for i in 0..<datesData.count {
+            yVals1.append(ChartDataEntry(x: Double(i), y: priceData[i]))
+        }
+        
+        // 2 - create a data set with our array
+        let set1: LineChartDataSet = LineChartDataSet(values: yVals1, label: "First Set")
+        
+        //            let set2: LineChartDataSet = LineChartDataSet(values: yVals2, label: "Second Set")
+        
+        set1.axisDependency = .left // Line will correlate with left axis values
+        set1.setColor(UIColor.red.withAlphaComponent(0.5)) // our line's opacity is 50%
+        set1.setCircleColor(UIColor.red) // our circle will be dark red
+        set1.lineWidth = 2.0
+        set1.circleRadius = 6.0 // the radius of the node circle
+        set1.fillAlpha = 65 / 255.0
+        set1.fillColor = UIColor.red
+        set1.highlightColor = UIColor.gray
+        set1.drawCircleHoleEnabled = true
+        
+        //3 - create an array to store our LineChartDataSets
+        var dataSets : [LineChartDataSet] = [LineChartDataSet]()
+        dataSets.append(set1)
+        
+        //4 - pass our months in for our x-axis label value along with our dataSets
+        let data: LineChartData = LineChartData(dataSets: dataSets)
+        data.setValueTextColor(UIColor.blue)
+        
+        //5 - finally set our data
+        self.lineChartView.data = data
+        
+        //6 - add x-axis label
+        let xaxis = self.lineChartView.xAxis
+        xaxis.valueFormatter = MyXAxisFormatter(months)
+        
+    }
+
 
 
 }
 
+
+class MyXAxisFormatter: NSObject, IAxisValueFormatter {
+    
+    let months: [String]
+    
+    init(_ months: [String]) {
+        self.months = months
+    }
+    
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        return months[Int(value)]
+}
+
+    class MyXAxisFormatter: NSObject, IAxisValueFormatter {
+        
+        let dates: [String]
+        
+        init(_ dates: [String]) {
+            self.dates = dates
+        }
+        
+        func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+            return dates[Int(value)]
+        }
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'CompareKart-v1' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+pod ‘Charts’
+  	pod 'SwiftyJSON'
+ pod 'Alamofire', '~> 4.4'	
+pod 'SDWebImage', '~>3.8'
+
+  # Pods for CompareKart-v1
+
+  target 'CompareKart-v1Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CompareKart-v1UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
