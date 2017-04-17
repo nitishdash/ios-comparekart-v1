bb83921c5fbfb58e92a74ae87e02578cd6278172
Nitish Dash <nitishdash@Nitishs-Mac.local>
2017-04-17 12:31:36 -0700

    Second major commit - Fixed graph, added more functionality

---
+++ CompareKart-v1/InitialVC.swift
@@ -0,0 +1,26 @@
+//
+//  InitialVC.swift
+//  CompareKart-v1
+//
+//  Created by Nitish Dash on 16/04/17.
+//  Copyright © 2017 Nitish Dash. All rights reserved.
+//
+
+import UIKit
+
+class InitialVC: UIViewController {
+    
+    @IBOutlet weak var segueButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    
+        
+    }
+    
+    
+    
+    
+}
--- CompareKart-v1/ViewController.swift
+++ CompareKart-v1/ViewController.swift
@@ -13,7 +13,12 @@ import Charts
 
 
 class ViewController: UIViewController , ChartViewDelegate {
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var lowestPriceLabel: UILabel!
+    @IBOutlet weak var trackingSinceLabel: UILabel!
 
+    @IBOutlet weak var lowestPriceOnLabel: UILabel!
+    @IBOutlet weak var currentPriceLabel: UILabel!
     @IBOutlet weak var lineChartView: LineChartView!
     
     let months = ["Jan" , "Feb", "Mar", "Apr", "May", "June", "July", "August", "Sept", "Oct", "Nov", "Dec"]
@@ -23,7 +28,12 @@ class ViewController: UIViewController , ChartViewDelegate {
     var priceData = [0.00]
     
     var datesData = [""]
+    
+    var lowestPrice = 0.00
+    var lowestPriceOn = ""
  
+    var currentPrice = 0.00
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -41,15 +51,61 @@ class ViewController: UIViewController , ChartViewDelegate {
                 
                 print(json)
                 
-                 let count: Int? = json.array?.count
+                 let count: Int? = json["trackingDetails"].array?.count   //check for the maximum count in json array
+                
+                var min = 0.00  //trash variable for checking minimum price
+
                 
                 for index in 0...count!-1 {
                     
-                    self.priceData.append(json[index]["price"].doubleValue)
+                    if index == 0 {
+                        min = json["trackingDetails"][0]["price"].doubleValue
+                    }
                     
-                    self.datesData.append(json[index]["Name"].stringValue)
+                    //determining lowest price of product :P No ALGO!
+                    if min>json["trackingDetails"][index]["price"].doubleValue {
+                        min=json["trackingDetails"][index]["price"].doubleValue
+                        
+                        self.lowestPriceOn = json["trackingDetails"][index]["date"].stringValue
+                    }
+                    
+                    self.priceData.append(json["trackingDetails"][index]["price"].doubleValue)
+                    
+                    self.datesData.append(json["trackingDetails"][index]["Name"].stringValue)
+                    
                 }
                 
+                //set the product name
+                self.productNameLabel.text=json["productDetails"][0]["title"].stringValue
+                
+                
+                
+                
+                let str=json["productDetails"][0]["dateOfAddition"].stringValue
+                
+                
+                let index = str.index(str.startIndex, offsetBy: 10)
+                
+                
+                
+                
+                self.trackingSinceLabel.text=str.substring(to: index)
+                
+                
+                
+                let index2 = self.lowestPriceOn.index(str.startIndex, offsetBy: 10)
+                
+                
+                self.lowestPriceOnLabel.text = "on: "+self.lowestPriceOn.substring(to: index2)
+                
+                
+                self.currentPrice = json["trackingDetails"][0]["mrp"].doubleValue
+                self.lowestPrice = min
+                
+                self.currentPriceLabel.text = String(self.currentPrice)
+                
+                
+                self.lowestPriceLabel.text = String(self.lowestPrice)
                 
                 // 1
                 self.lineChartView.delegate = self
@@ -57,11 +113,13 @@ class ViewController: UIViewController , ChartViewDelegate {
                 self.lineChartView.chartDescription?.text = "Tap node for details"
                 // 3
                 self.lineChartView.chartDescription?.textColor = UIColor.black
-                self.lineChartView.gridBackgroundColor = UIColor.darkGray
+                self.lineChartView.gridBackgroundColor = UIColor.clear
                 // 4
                 self.lineChartView.noDataText = "No data provided"
                 // 5
                 self.setChartData(months: self.datesData)
+                
+                
                 
                 
 
@@ -140,18 +198,18 @@ class MyXAxisFormatter: NSObject, IAxisValueFormatter {
     func stringForValue(_ value: Double, axis: AxisBase?) -> String {
         return months[Int(value)]
 }
-
-    class MyXAxisFormatter: NSObject, IAxisValueFormatter {
-        
-        let dates: [String]
-        
-        init(_ dates: [String]) {
-            self.dates = dates
-        }
-        
-        func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-            return dates[Int(value)]
-        }
-    
-    
 }
+//    class MyXAxisFormatter: NSObject, IAxisValueFormatter {
+//        
+//        let dates: [String]
+//        
+//        init(_ dates: [String]) {
+//            self.dates = dates
+//        }
+//        
+//        func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+//            return dates[Int(value)]
+//        }
+//    
+//    
+//}
